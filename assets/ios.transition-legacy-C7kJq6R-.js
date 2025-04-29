232f98107a19c702af639aca00466a901b64f98c
Asnairah <Abdulasizderon@gmail.com>
2025-04-29 17:11:09 +0800

    Updates

--- assets/ios.transition-legacy-C7kJq6R-.js
+++ assets/ios.transition-legacy-C7kJq6R-.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dp-E3z0E.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-XQF3yBCL.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-DMoYYBIE.js
+++ assets/swipe-back-legacy-DMoYYBIE.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dp-E3z0E.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-XQF3yBCL.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
