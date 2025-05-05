5a7cf768bc975396b35e03e61358ed587c111289
Asnairah <abdulasizderon@gmail.com>
2025-05-05 10:52:24 +0800

    Updates

--- assets/ios.transition-legacy-fEgHclhn.js
+++ assets/ios.transition-legacy-fEgHclhn.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-XQF3yBCL.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-XHumyzqm.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-BmcPOiX2.js
+++ assets/md.transition-legacy-BmcPOiX2.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-XQF3yBCL.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-XHumyzqm.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
